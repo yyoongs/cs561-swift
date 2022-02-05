315fae13edec9773834fcbb907e0beeee139f11d
yyoongs <jysid3531@gmail.com>
2022-02-05 00:08:12 -0800

    edit swift client

--- MyLibrary/Sources/MyLibrary/MyLibrary.swift
+++ MyLibrary/Sources/MyLibrary/MyLibrary.swift
@@ -1,6 +1,8 @@
 public class MyLibrary {
     private let weatherService: WeatherService
-
+    private let AuthService: AuthService = AuthServiceImpl()
+    private let v1Weather: v1Weather = v1WeatherImpl()
+    private let v1Hello: v1Hello = v1HelloImpl()
     /// The class's initializer.
     ///
     /// Whenever we call the `MyLibrary()` constructor to instantiate a `MyLibrary` instance,
@@ -42,4 +44,44 @@ public class MyLibrary {
     private func contains(_ lhs: Int, _ rhs: Character) -> Bool {
         return String(lhs).contains(rhs)
     }
+    
+    public func getAuth(completion: @escaping (String?) -> Void){
+        AuthService.getAccessToken { response in
+            switch response {
+            case let .failure(error):
+                print(error)
+                completion(nil)
+
+            case let .success(jwt):
+                completion(jwt)
+            }
+        }
+    }
+    
+    public func getWeather(completion: @escaping (String?) -> Void){
+        v1Weather.getWeather { response in
+            switch response {
+            case let .failure(error):
+                print(error)
+                completion(nil)
+
+            case let .success(temp):
+                completion(temp)
+            }
+        }
+    }
+    
+    public func getHello(completion: @escaping (String?) -> Void){
+        v1Hello.getHello { response in
+            switch response {
+            case let .failure(error):
+                print(error)
+                completion(nil)
+
+            case let .success(message):
+                completion(message)
+            }
+        }
+    }
+    
 }
